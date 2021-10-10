4210707cc4677baf7ea7e2b9396d73ca4d1a5416
rwagatenga <fredrwagatenga@gmail.com>
2021-10-11 00:13:35 +0200

    advanced component concept

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12">
-				<app-user></app-user>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+                <!--<app-quote>-->
+                    <!--<h2 slot="title">{{ quoteTitle }}</h2>-->
+                    <!--<p>A wonderful Quote</p>-->
+                <!--</app-quote>-->
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	import User from './components/User.vue';
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
-	export default {
-		components: {
-			appUser: User
-		}
-	}
+    export default {
+        data: function() {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
 </script>
 
 <style>
-	div.component {
-		border: 1px solid black;
-		padding: 30px;
-	}
 </style>
