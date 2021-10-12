ce0b50e2a2c6310d0c159108534a7764c25b0061
rwagatenga <fredrwagatenga@gmail.com>
2021-10-12 11:50:20 +0200

    concept of form handler

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,173 @@
 <template>
     <div class="container">
-		<app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-		<app-new-quote @quoteAdded="newQuote"></app-new-quote>
-		<app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
-		<div class="row">
-			<div class="col-sm-12 text-center">
-				<div class="alert alert-info">
-					Info: Click on a Quote to Delete it !!
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+								v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+								v-model="userData.password">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control"
+								v-model="userData.age">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control"
+							v-model="message"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail"
+									v-model="sendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"
+									v-model="sendMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male"
+								v-model="gender"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female"
+								v-model="gender"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control"
+							v-model="selectedPriority">
+                        <option 
+							v-for="(priority, index) in priorities"
+							:key="index"> 
+							{{ priority }}
+						</option>
+                    </select>
+                </div>
+            </div>
+			<div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+					<app-switch v-model="dataSwitch"></app-switch>
 				</div>
 			</div>
-		</div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary"
+							@click.prevent="submitting">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="submitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password}} </p>
+                        <p>Age: {{ userData.age }} </p>
+                        <p style="white-space: pre">Message: </p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li v-for="(send, index) in sendMail" :key="index">{{ send }}</li>
+                        </ul>
+                        <p>Gender: {{ gender }} </p>
+                        <p>Priority: {{ selectedPriority }}</p>
+                        <p>Switched: {{ dataSwitch}} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-	import QuoteGrid from "./components/QuoteGrid.vue"
-	import NewQuote from "./components/NewQuote.vue"
-	import Header from "./components/Header.vue"
+	import Switch from "./Switch.vue"
     export default {
-        data: function() {
-			return { 
-				quotes: [
-					'Just a Quote to see something'
-				],
-				maxQuotes: 10
+		data() {
+			return {
+				userData: {
+					email: '',
+					password: '',
+					age: ''
+				},
+				message: 'Something Else',
+				sendMail: [],
+				gender: '',
+				selectedPriority: 'Medium',
+				priorities: ['High', 'Medium', 'Low'],
+				dataSwitch: true,
+				submitted: false
 			}
 		},
 		methods: {
-			newQuote (quote) {
-				if (this.quotes.length >= this.maxQuotes) {
-					return alert("Please Delete Quotes !!")
-				}
-				this.quotes.push(quote)
-			},
-			deleteQuote(index) {
-				this.quotes.splice(index, 1)
+			submitting () {
+				this.submitted = true
 			}
 		},
 		components: {
-			appQuoteGrid: QuoteGrid,
-			appNewQuote: NewQuote,
-			appHeader: Header
-		}
+			appSwitch: Switch
+		},
     }
 </script>
 
 <style>
+
 </style>
