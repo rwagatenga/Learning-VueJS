b8a786f22e20ac15e0708b2b35d69f840fd7d99e
rwagatenga <fredrwagatenga@gmail.com>
2021-10-12 13:32:06 +0200

    concepts of directives

--- src/App.vue
+++ src/App.vue
@@ -1,171 +1,53 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <h1>File a Complaint</h1>
-                    <hr>
-                    <div class="form-group">
-                        <label for="email">Mail</label>
-                        <input
-                                type="text"
-                                id="email"
-                                class="form-control"
-								v-model="userData.email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input
-                                type="password"
-                                id="password"
-                                class="form-control"
-								v-model="userData.password">
-                    </div>
-                    <div class="form-group">
-                        <label for="age">Age</label>
-                        <input
-                                type="number"
-                                id="age"
-                                class="form-control"
-								v-model="userData.age">
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="message">Message</label><br>
-                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea
-                            id="message"
-                            rows="5"
-                            class="form-control"
-							v-model="message"></textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <div class="form-group">
-                        <label for="sendmail">
-                            <input
-                                    type="checkbox"
-                                    id="sendmail"
-                                    value="SendMail"
-									v-model="sendMail"> Send Mail
-                        </label>
-                        <label for="sendInfomail">
-                            <input
-                                    type="checkbox"
-                                    id="sendInfomail"
-                                    value="SendInfoMail"
-									v-model="sendMail"> Send Infomail
-                        </label>
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
-                        <input
-                                type="radio"
-                                id="male"
-                                value="Male"
-								v-model="gender"> Male
-                    </label>
-                    <label for="female">
-                        <input
-                                type="radio"
-                                id="female"
-                                value="Female"
-								v-model="gender"> Female
-                    </label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-                    <label for="priority">Priority</label>
-                    <select
-                            id="priority"
-                            class="form-control"
-							v-model="selectedPriority">
-                        <option 
-							v-for="(priority, index) in priorities"
-							:key="index"> 
-							{{ priority }}
-						</option>
-                    </select>
-                </div>
-            </div>
-			<div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-					<app-switch v-model="dataSwitch"></app-switch>
-				</div>
-			</div>
-            <hr>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <button
-                            class="btn btn-primary"
-							@click.prevent="submitting">Submit!
-                    </button>
-                </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row" v-if="submitted">
+        <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Mail: {{ userData.email }}</p>
-                        <p>Password: {{ userData.password}} </p>
-                        <p>Age: {{ userData.age }} </p>
-                        <p style="white-space: pre">Message: </p>
-                        <p><strong>Send Mail?</strong></p>
-                        <ul>
-                            <li v-for="(send, index) in sendMail" :key="index">{{ send }}</li>
-                        </ul>
-                        <p>Gender: {{ gender }} </p>
-                        <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched: {{ dataSwitch}} </p>
-                    </div>
-                </div>
+                <h1>Built-in Directives</h1>
+				<p v-text="'Some Text'"></p>
+				<p v-html="'<strong>Some Text in Strong Tag</strong>'"></p>
+            </div>
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+				<p v-custom>Custom Directve 1 with v-custom </p>
+				<p v-custom="'red'">Custom Directve 1 with v-custom="'red'"</p>
+				<p v-custom:background="'blue'">Custom Directve 1 with v-custom:background="'blue'"</p>
+				<p v-custom:background.delayed="'black'">Custom Directve modifier delayed at 3secs </p>
+				<p v-local-custom:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 1000}">Registed Directive</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-	import Switch from "./Switch.vue"
-    export default {
-		data() {
-			return {
-				userData: {
-					email: '',
-					password: '',
-					age: ''
-				},
-				message: 'Something Else',
-				sendMail: [],
-				gender: '',
-				selectedPriority: 'Medium',
-				priorities: ['High', 'Medium', 'Low'],
-				dataSwitch: true,
-				submitted: false
+	export default {
+		directives: {
+			'local-custom': {
+				bind(el, binding, vnode) {
+					var delay = 0;
+					if (binding.modifiers['delayed']) {
+						delay = 3000
+					}
+					if (binding.modifiers['blink']) {
+						let mainColor = binding.value.mainColor
+						let secondColor = binding.value.secondColor
+						let currentColor = mainColor
+						setTimeout(() => {
+							setInterval(() => {
+								currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor
+								if (binding.arg == 'background') {
+									el.style.backgroundColor = currentColor
+								}
+							}, binding.value.delay);
+						}, delay);
+					} else {
+						setTimeout(() => {
+							el.style.backgroundColor = binding.value.mainColor
+						}, delay);
+					}
+				}
 			}
-		},
-		methods: {
-			submitting () {
-				this.submitted = true
-			}
-		},
-		components: {
-			appSwitch: Switch
-		},
-    }
+		}
+	}
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('custom', {
+	bind(el, binding, vnode) {
+		el.style.backgroundColor = "green"
+		el.style.backgroundColor = binding.value
+		if (binding.arg == 'background') {
+			el.style.backgroundColor = "blue"
+		}
+		var delay = 0
+		if (binding.modifiers['delayed']) {
+			delay = 3000;
+		}
+		setTimeout(() => {
+			el.style.backgroundColor = binding.value
+		}, delay);
+	}
+})
 new Vue({
 	el: '#app',
 	render: h => h(App)
